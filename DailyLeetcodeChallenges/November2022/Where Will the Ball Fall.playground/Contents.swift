4220cf0e424d2377be9480db613794b190d86833
Pritam Dash <pritamdash2015@outlook.com>
2022-11-01 14:08:11 +0530

    Where will the ball fall solved

--- DailyLeetcodeChallenges/November2022/Where Will the Ball Fall.playground/Contents.swift
+++ DailyLeetcodeChallenges/November2022/Where Will the Ball Fall.playground/Contents.swift
@@ -12,8 +12,67 @@ import Foundation
 
  Return an array answer of size n where answer[i] is the column that the ball falls out of at the bottom after dropping the ball from the ith column at the top, or -1 if the ball gets stuck in the box.
  */
+//fast solution
 class Solution {
     func findBall(_ grid: [[Int]]) -> [Int] {
-        
+        var res = [Int]()
+        let rows = grid.endIndex, cols = grid[0].endIndex
+        next: for i in 0..<cols {
+            var currRow = 0, currCol = i
+            while currRow < rows {
+                if grid[currRow][currCol] == 1 {
+                    guard currCol + 1 < cols else {
+                        res.append(-1)
+                        continue next
+                    }
+                    if grid[currRow][currCol+1] == 1 {
+                        currRow += 1
+                        currCol += 1
+                    } else {
+                        res.append(-1)
+                        continue next
+                    }
+                } else {
+                    guard currCol-1 >= 0 else {
+                        res.append(-1)
+                        continue next
+                    }
+                    if grid[currRow][currCol-1] == -1 {
+                        currRow += 1
+                        currCol -= 1
+                    } else {
+                        res.append(-1)
+                        continue next
+                    }
+                }
+            }
+            res.append(currCol)
+        }
+        return res
+    }
+}
+
+//faster solution
+class Solution2 {
+    func findBall(_ grid: [[Int]]) -> [Int] {
+            let cols = grid[0].endIndex
+            var result = [Int]()
+            for j in 0..<cols{
+                result.append(dfs(0, j, grid))
+            }
+            return result
+        }
+    
+    func dfs(_ i :Int, _ j : Int, _ grid : [[Int]]) -> Int{
+        if i >= grid.endIndex { return j }
+        if grid[i][j] == 1 && j+1 < grid[0].endIndex && grid[i][j+1] == 1 {
+            return dfs(i+1, j+1, grid)
+        }else if grid[i][j] == -1 && j-1 >= 0 && grid[i][j-1] == -1{
+            return dfs(i+1, j-1, grid)
+        }else if grid[i][j] == 1 && j+1 >= grid[0].endIndex{
+            return -1
+        }else{
+            return -1
+        }
     }
 }
