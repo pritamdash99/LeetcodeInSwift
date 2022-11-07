ef5fe5044fe61808a6c4ed4d59e679025ed5dcd5
Pritam Dash <pritamdash2015@outlook.com>
2022-11-07 21:54:39 +0530

    Maximum 69 number solved

--- DailyLeetcodeChallenges/November2022/Maximum 69 Number .playground/Contents.swift
+++ DailyLeetcodeChallenges/November2022/Maximum 69 Number .playground/Contents.swift
@@ -32,5 +32,93 @@ import Foundation
 class Solution {
     func maximum69Number (_ num: Int) -> Int {
         
+        let numstr = String(num)
+        var nums = Array(numstr)
+        for i in 0..<nums.count{
+            if nums[i] == "6" {
+                nums[i] = "9"
+                let x = String(nums)
+                return Int(x) ?? 0
+            }
+        }
+        
+        return num
     }
 }
+
+let x = Solution()
+print(x.maximum69Number(69))
+
+//fastest solution but uses space
+
+class Solution2 {
+    func maximum69Number (_ num: Int) -> Int {
+        let stringRep = String(num)
+        
+        // First index has index of the first 6
+        var firstIndex = -1
+        
+        for (index, n) in stringRep.enumerated() {
+            if n == "6" {
+                firstIndex = index
+                break
+            }
+        }
+        
+        if firstIndex != -1 {
+            let ss1 = stringRep.prefix(firstIndex)
+            let ss2 = stringRep.suffix(stringRep.count - firstIndex - 1)
+            let result = ss1 + "9" + ss2
+            return Int(result)!
+        }
+        return num
+    }
+}
+
+//faster solution
+class Solution3 {
+    func maximum69Number (_ num: Int) -> Int {
+        var count = 0
+        let nums = String(num)
+        var total: [Character] = []
+        for n in nums {
+            if n == "6" && count == 0 {
+                total.append("9")
+                count += 1
+            } else {
+                total.append(n)
+            }
+        }
+        let result = Int(String(total))!
+        return result
+    }
+}
+
+//fastest solution taking minimum space
+
+class Solution4 {
+    func maximum69Number (_ num: Int) -> Int {
+        // Since we start with the lowest digit, initialize currDigit = 0.
+        var numCopy = num
+        var indexSix = -1
+        var currentDigit = 0
+        
+        // Check every digit of 'numCopy' from low to high.
+        while numCopy > 0 {
+            // If the current digit is '6', record it as the highest digit of 6
+            if numCopy % 10 == 6 {
+                indexSix = currentDigit
+            }
+            
+            // Move on to the next digit.
+            numCopy /= 10
+            currentDigit += 1
+        }
+        
+        // If we don't find any digit of '6', return the original number,
+        // otherwise, increment 'num' by the difference made by the first '6'.
+        return indexSix == -1 ? num : num + 3 * Int((pow(10.0, Double(indexSix))))
+    }
+}
+let y = Solution4()
+print(x.maximum69Number(69))
