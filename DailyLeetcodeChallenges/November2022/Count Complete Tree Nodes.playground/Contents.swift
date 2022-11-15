a8789a83e215ee90c4f4e1a7e37aa2dc52d2800c
Pritam Dash <pritamdash2015@outlook.com>
2022-11-15 20:59:22 +0530

    Count Complete Tree Nodes Created

---
+++ DailyLeetcodeChallenges/November2022/Count Complete Tree Nodes.playground/Contents.swift
@@ -0,0 +1,45 @@
+import UIKit
+/*
+ 222. Count Complete Tree Nodes
+ Given the root of a complete binary tree, return the number of the nodes in the tree.
+
+ According to Wikipedia, every level, except possibly the last, is completely filled in a complete binary tree, and all nodes in the last level are as far left as possible. It can have between 1 and 2h nodes inclusive at the last level h.
+
+ Design an algorithm that runs in less than O(n) time complexity.
+ 
+ Example 1:
+ 
+ Input: root = [1,2,3,4,5,6]
+ Output: 6
+
+ Example 2:
+
+ Input: root = []
+ Output: 0
+
+ Example 3:
+
+ Input: root = [1]
+ Output: 1
+
+ */
+//Definition for a binary tree node.
+
+  public class TreeNode {
+      public var val: Int
+      public var left: TreeNode?
+      public var right: TreeNode?
+      public init() { self.val = 0; self.left = nil; self.right = nil; }
+      public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+      public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+          self.val = val
+          self.left = left
+          self.right = right
+      }
+  }
+ 
+class Solution {
+    func countNodes(_ root: TreeNode?) -> Int {
+        
+    }
+}
