d3af49b19b23af27fa2238f9d2c8341becab6589
Pritam Dash <pritamdash2015@outlook.com>
2022-11-15 21:14:09 +0530

    Solved

--- DailyLeetcodeChallenges/November2022/Count Complete Tree Nodes.playground/Contents.swift
+++ DailyLeetcodeChallenges/November2022/Count Complete Tree Nodes.playground/Contents.swift
@@ -39,7 +39,119 @@ import UIKit
   }
  
 class Solution {
+    var counter = 0
     func countNodes(_ root: TreeNode?) -> Int {
+        guard let root = root else { return 0 }
+        inorderTraversal(root)
+        return counter
+    }
+    
+    private func inorderTraversal(_ node : TreeNode?){
+        guard let node = node else { return }
+        inorderTraversal(node.left)
+        counter += 1
+        inorderTraversal(node.right)
         
+    }
+}
+
+//Shorter Solution
+class Solution2 {
+    func countNodes(_ root: TreeNode?) -> Int {
+      if let root = root { return 1 + countNodes(root.left) + countNodes(root.right) }
+      return 0
+    }
+}
+
+//Faster solution
+/*
+         0        - level 0: 2^0
+       /   \
+      1     2     - level 1: 2^1
+     / \   / \
+    3   4 5   6   - level 2: 2^2    sum(level0...k-1): 2^k - 1
+    ... n         - level k: 2^k
+        ^: h = log2(n)
+
+    get depth
+    count the number in last level
+    use binaray search to find the latest index of last level
+
+    level K: numOfK / 2 until 1 -> level: 0
+
+        
+ */
+class Solution3 {
+    func countNodes(_ root: TreeNode?) -> Int {
+        guard let root = root else {
+            return 0
+        }
+        // print(root.node(at: 0, level: 0)?.val)
+        // print(root.node(at: 0, level: 1)?.val)
+        // print(root.node(at: 1, level: 1)?.val)
+        // print(root.node(at: 0, level: 2)?.val)
+        // print(root.node(at: 1, level: 2)?.val)
+        // print(root.node(at: 2, level: 2)?.val)
+        // print(root.node(at: 3, level: 2)?.val)
+        let depth = root.depth
+        let count = TreeNode.countOfLevel(depth)
+        let n = latestIndexOfTree(root, 0, count-1, height: depth)
+        // print("n \(n)")
+        return count + n // count - 1 + n + 1
+        
+        // return root.count
+    }
+    
+    func latestIndexOfTree(_ node: TreeNode, _ l: Int, _ r: Int, height: Int) -> Int {
+        guard l <= r else {
+            if r < l {
+                // 0 1 -> 0 1
+                // t        ^
+                return l - 1
+            } else {
+                // 0 1 -> 0 1
+                //   f    ^
+                return l
+            }
+        }
+        let mid = l + (r-l)/2
+        if node.node(at: mid, level: height) != nil {
+            return latestIndexOfTree(node, mid+1, r, height: height)
+        } else {
+            return latestIndexOfTree(node, l, mid-1, height: height)
+        }
+    }
+}
+
+extension TreeNode {
+    static func countOfLevel(_ level: Int) -> Int {
+        guard level >= 0 else {
+            return 0
+        }
+        return (pow(2, level) as NSDecimalNumber).intValue
+    }
+    
+    func node(at index: Int, level: Int) -> TreeNode? {
+        guard level > 0 else {
+            return index == 0 ? self : nil
+        }
+        let count = TreeNode.countOfLevel(level)
+        guard index < count else {
+            return nil
+        }
+        let mid = count / 2
+        if index < count / 2 {
+            return left?.node(at: index, level: level - 1)
+        } else {
+            return right?.node(at: index - mid, level: level - 1)
+        }
+    }
+    
+    var depth: Int {
+        (left?.depth ?? -1) + 1
+    }
+    
+    var count: Int {
+        (left?.count ?? 0) + (right?.count ?? 0) + 1
     }
 }
