8d89ec1c07ce05dd1cc24a0513bf74272bef39f0
Pritam Dash <pritamdash2015@outlook.com>
2022-11-18 20:14:52 +0530

    Ugly Number Solved

--- DailyLeetcodeChallenges/November2022/Ugly Number .playground/Contents.swift
+++ DailyLeetcodeChallenges/November2022/Ugly Number .playground/Contents.swift
@@ -69,20 +69,16 @@ class Solution2 {
         } else if n < 1 {
             return false
         }
-
         var num = n
         while (num % 2 == 0) {
             num /= 2
         }
-
         while (num % 3 == 0) {
             num /= 3
         }
-
         while (num % 5 == 0) {
             num /= 5
         }
-
         return num == 1
     }
 }
