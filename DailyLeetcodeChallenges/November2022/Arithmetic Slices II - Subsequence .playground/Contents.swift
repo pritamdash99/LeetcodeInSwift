0504415fa1277f5396b96a1c27677b688f6be847
Pritam Dash <pritamdash2015@outlook.com>
2022-11-27 19:57:15 +0530

    Solved

--- DailyLeetcodeChallenges/November2022/Arithmetic Slices II - Subsequence .playground/Contents.swift
+++ DailyLeetcodeChallenges/November2022/Arithmetic Slices II - Subsequence .playground/Contents.swift
@@ -44,6 +44,24 @@ import Foundation
 
 class Solution {
     func numberOfArithmeticSlices(_ nums: [Int]) -> Int {
-        
+        var res = 0
+        var map = [[Int: Int]](repeating: [:], count: nums.count)
+        for i in 0..<nums.endIndex {
+            for j in 0..<i {
+                let diff = nums[j] - nums[i]
+                let c1 = map[i][diff, default:0]
+                let c2 = map[j][diff, default:0]
+                res += c2
+                map[i][diff,default:0] = c1+c2+1
+            }
+        }
+        return res
     }
 }
+
+let x = Solution()
+print(x.numberOfArithmeticSlices([2,4,6,8,10]))
+/*
+ o/p : 7
+ 
+ */
