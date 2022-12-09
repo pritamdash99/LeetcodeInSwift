23b5e7379306bbcab6c2dbec5f4b0b7ea39d2236
Pritam Dash <pritamdash2015@outlook.com>
2022-12-09 22:00:06 +0530

    Maximum differences between node and ancestor created

---
+++ DailyLeetcodeChallenges/December2022/Maximum Difference Between Node and Ancestor.playground/Contents.swift
@@ -0,0 +1,48 @@
+/*
+ 1026. Maximum Difference Between Node and Ancestor
+
+ Given the root of a binary tree, find the maximum value v for which there exist different nodes a and b where v = |a.val - b.val| and a is an ancestor of b.
+
+ A node a is an ancestor of b if either: any child of a is equal to b or any child of a is an ancestor of b.
+
+ Example 1:
+ Input: root = [8,3,10,1,6,null,14,null,null,4,7,13]
+ Output: 7
+ Explanation: We have various ancestor-node differences, some of which are given below :
+ |8 - 3| = 5
+ |3 - 7| = 4
+ |8 - 1| = 7
+ |10 - 13| = 3
+ Among all possible differences, the maximum value of 7 is obtained by |8 - 1| = 7.
+ 
+ Example 2 :
+ 
+ Input: root = [1,null,2,null,0,3]
+ Output: 3
+
+ Constraints:
+
+     The number of nodes in the tree is in the range [2, 5000].
+     0 <= Node.val <= 10^5
+
+ */
+import Foundation
+
+public class TreeNode {
+    public var val: Int
+    public var left: TreeNode?
+    public var right: TreeNode?
+    public init() { self.val = 0; self.left = nil; self.right = nil; }
+    public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+    public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+        self.val = val
+        self.left = left
+        self.right = right
+    }
+}
+
+class Solution {
+    func maxAncestorDiff(_ root: TreeNode?) -> Int {
+        
+    }
+}
