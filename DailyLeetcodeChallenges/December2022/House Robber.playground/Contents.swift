003c33b9175a4d4fb381243019eb76d36766ccb1
Pritam Dash <pritamdash2015@outlook.com>
2022-12-14 22:28:18 +0530

    Solved

--- DailyLeetcodeChallenges/December2022/House Robber.playground/Contents.swift
+++ DailyLeetcodeChallenges/December2022/House Robber.playground/Contents.swift
@@ -29,6 +29,15 @@ import Foundation
 
 class Solution {
     func rob(_ nums: [Int]) -> Int {
-        
+        var rob1 = 0
+        var rob2 = 0
+
+        for num in nums {
+            let temp = max(num + rob1, rob2)
+            rob1 = rob2
+            rob2 = temp
+        }
+        return rob2
     }
 }
+
