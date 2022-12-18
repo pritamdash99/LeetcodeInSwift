862557700c5f0ad017f41d5ce6f76e187ba68421
Pritam Dash <pritamdash2015@outlook.com>
2022-12-18 19:05:11 +0530

    solved

--- DailyLeetcodeChallenges/December2022/Daily Temperatures .playground/Contents.swift
+++ DailyLeetcodeChallenges/December2022/Daily Temperatures .playground/Contents.swift
@@ -29,7 +29,22 @@
 import Foundation
 
 class Solution {
-    func dailyTemperatures(_ temperatures: [Int]) -> [Int] {
-        
+      func dailyTemperatures(_ temperatures: [Int]) -> [Int] {
+        var result = Array(repeating: 0, count: temperatures.count)
+        var stack = [Int]()
+        var index = 0
+        while index < temperatures.count {
+            while !stack.isEmpty && temperatures[stack.last!] < temperatures[index] {
+                let prevDay = stack.last!
+                result[prevDay] = index - prevDay
+                stack.removeLast()
+            }
+            stack.append(index)
+            index += 1
+        }
+        return result
     }
 }
+
+
+
