77c4103c6aad2b6d3b4ff58827134f108309f483
Pritam Dash <pritamdash2015@outlook.com>
2022-12-22 21:22:40 +0530

    solved

--- DailyLeetcodeChallenges/December2022/Sum of Distances in Tree .playground/Contents.swift
+++ DailyLeetcodeChallenges/December2022/Sum of Distances in Tree .playground/Contents.swift
@@ -38,6 +38,28 @@ import Foundation
 
 class Solution {
     func sumOfDistancesInTree(_ n: Int, _ edges: [[Int]]) -> [Int] {
+        var result = Array(repeating: 0, count: n)
+        var count = Array(repeating: 1, count: n)
+        let graph = edges.reduce(into: Array(repeating: [Int](), count: n)) { graph, edge in
+            graph[edge[0]].append(edge[1])
+            graph[edge[1]].append(edge[0])
+        }
+
+        func dfs(pass: Int, _ node:Int = 0, _ parent: Int = -1) {
+            for child in graph[node] where child != parent {
+                if pass == 2 {
+                    result[child] = (result[node] + n) - 2 * count[child]
+                }
+                dfs(pass: pass, child, node)
+                if pass == 1 {
+                    count[node] += count[child]
+                    result[node] += result[child] + count[child]
+                }
+            }
+        }
         
+        dfs(pass: 1)
+        dfs(pass: 2)
+        return result
     }
 }
