3b4084af08f0625d4bce8fa13426f9276a28ce40
Pritam Dash <pritamdash2015@outlook.com>
2023-01-02 23:08:48 +0530

    Solved

--- DailyLeetcodeChallenges/January2023/Detect Capital.playground/Contents.swift
+++ DailyLeetcodeChallenges/January2023/Detect Capital.playground/Contents.swift
@@ -29,6 +29,35 @@ import Foundation
 
 class Solution {
     func detectCapitalUse(_ word: String) -> Bool {
-        
+        word == word.uppercased() || word == word.capitalized || word == word.lowercased()
+    }
+}
+
+class Solution2 {
+    func detectCapitalUse(_ word: String) -> Bool {
+        var countCase1 = 0
+        var countCase2 = 0
+        var firstCharUpper = false
+        var firstTime = true
+
+        let rangeUpper = 65..<91
+        let rangeLower = 97..<123
+
+        for i in word {
+            var ascii = Int(i.asciiValue!)
+            if rangeUpper.contains(ascii) {
+                countCase1 += 1
+                if firstTime {
+                    firstCharUpper = true
+                }
+            } else if rangeLower.contains(ascii) {
+                countCase2 += 1
+            }
+            firstTime = false
+        }
+        print("countCase2: \(countCase2) firstCharUpper:\(firstCharUpper) word.count:\(word.count)")
+        if word.count == countCase1 || word.count == countCase2 { return true }
+        if word.count-1 == countCase2 && firstCharUpper { return true }
+        return false
     }
 }
